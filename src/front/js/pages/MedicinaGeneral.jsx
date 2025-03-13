746790f0728e8dd552578d36d3b41d3be359d80c
Tavoide <gustavobenitez116@gmail.com>
2025-03-13 02:51:50 +0000

    fix flux

--- src/front/js/pages/MedicinaGeneral.jsx
+++ src/front/js/pages/MedicinaGeneral.jsx
@@ -6,51 +6,54 @@ const doctores = [
     { id: 2, name: "Doctora Yarely Martinez", especialidad: "Pediatra" },
 ];
 
-const MedicinaGeneral = () => { 
+const MedicinaGeneral = () => {
     const { store, actions } = useContext(Context);
 
 
 
 
-        const doctor = async () => {
-            try {
-              await actions.doctorsGet() 
-            } catch (error) {
-              console.error(error);
-            }
-          }
-        useEffect(()=>{doctor()
-            
-        },[])
+    const doctor = async () => {
+        try {
+            await actions.doctorsGet()
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        doctor()
+
+    }, [])
     return (
         <div>
             <h2>Medicina General</h2>
-          
-              <ul>
-            {Array.isArray(store.doctor) && store.doctor.filter((item) => item.specialty === "Medicina General").map((item) => (
-                <li className="card card-body my-3" key={item.id}>{item.name} - {item.email}- {item.specialty}</li>
-            ))}
-        </ul> 
 
-            </ul> 
-            <div> 
             <ul>
-            {Array.isArray (store.doctor) && store.doctor.filter((item) => item.specialty === "Medicina General").map((item) => (
-                <li style={{
-                    listStyleType: "none", 
-                    padding: "50px", 
-                    border: "1px solid #ddd", 
-                    borderRadius: "5px", 
-                    marginBottom: "10px",
-                }} key={item.id}><h3> {item.name} </h3> {item.email}  {item.specialty}</li>
-             </ul>
-           
-              <ul>
-            {store.doctor && store.doctor.map((item) => (
-                <li key={item.id}>{item.name} - {item.email}- {item.specialty}</li>
+                {Array.isArray(store.doctor) && store.doctor.filter((item) => item.specialty === "Medicina General").map((item) => (
+                    <li className="card card-body my-3" key={item.id}>{item.name} - {item.email}- {item.specialty}</li>
+                ))}
+            </ul>
 
-            ))}
-        </ul> 
+
+            <div>
+                <ul>
+                    {Array.isArray(store.doctor) && store.doctor.filter((item) => item.specialty === "Medicina General").map((item) => (
+                        <li style={{
+                            listStyleType: "none",
+                            padding: "50px",
+                            border: "1px solid #ddd",
+                            borderRadius: "5px",
+                            marginBottom: "10px",
+                        }} key={item.id}><h3> {item.name} </h3> {item.email}  {item.specialty}</li>
+                 
+                ))}
+            </ul>
+
+            <ul>
+                    {store.doctor && store.doctor.map((item) => (
+                        <li key={item.id}>{item.name} - {item.email}- {item.specialty}</li>
+
+                    ))}
+                </ul>
             </div>
 
         </div>
