831ebf2e358d463d89391dd9c60b796b82e152dc
Neverentherthesun <diego_28c@hotmail.com>
2025-03-21 05:10:32 +0000

    mas estilo y fincionalidad

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
-
+const navigate = useNavigate ()
     return (
         <div className="container">
-            <div className="card w-50 text-bg-dark">
-                <img src="https://media.istockphoto.com/id/517051420/es/foto/desconocido-retrato-de-un-hombre-sosteniendo-un-estetoscopio-m%C3%A9dico-un.jpg?s=612x612&w=0&k=20&c=OfOND4qSom_xEC_IuyHKf9pKO4H6z9wWTzCFpUJY0Ec=" 
-                className="card-img" alt="imagen"/>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">MEDagenda</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small>Los mejores profesionales a tu disposicion</small></p>
+            <div className="card w-50 text-bg-white border-white">
+                <div><img style={{paddingTop:"20px", marginLeft:"647px"}} src ="https://growmedical.org/wp-content/uploads/2019/01/Paginas-web-para-medicos.jpg" 
+                className="card-img" alt="imagen"/></div>
+                    <div className="card-img-overlay" style={{paddingTop:"60PX"}}>
+                    <h1> Cuidamos tu salud con profesionalismo</h1>
+                    <h5>Agenda tu cita médica de forma rápida y sencilla con los mejores especialistas.</h5>
+                    <button className="btn btn-primary btn-lg" onClick={()=> navigate("/registroPacientes")}>Agendar</button>
                     </div>
+                   
             </div>
 
         </div>
@@ -21,3 +23,6 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
