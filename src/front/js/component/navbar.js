2bd9245a871796d250e80fd4ac7929ee77da3578
Neverentherthesun <diego_28c@hotmail.com>
2025-03-21 23:57:34 +0000

    mas detalles agregados

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -42,7 +42,7 @@ export const Navbar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="60" height="50" fill="currentColor" class="bi bi-hospital" viewBox="0 0 16 16">
                         <path d="M8.5 5.034v1.1l.953-.55.5.867L9 7l.953.55-.5.866-.953-.55v1.1h-1v-1.1l-.953.55-.5-.866L7 7l-.953-.55.5-.866.953.55v-1.1zM13.25 9a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25zM13 11.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25zm.25 1.75a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25zm-11-4a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 9.75v-.5A.25.25 0 0 0 2.75 9zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25zM2 13.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25z" />
                         <path d="M5 1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1zm2 14h2v-3H7zm3 0h1V3H5v12h1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1zm0-14H6v1h4zm2 7v7h3V8zm-8 7V8H1v7z" />
-                    </svg> NovaMed
+                    </svg> NovaMed <i class="fa-solid fa-staff-snake"></i>
                 </Link>
                 <button
                     className="navbar-toggler"
@@ -59,7 +59,7 @@ export const Navbar = () => {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <span style={{fontSize:"14px", marginLeft:"100px", color:"white"}}>¡Seguimos cuidando tu salud! Recuerda: el uso de cubrebocas es obligatorio durante tu estancia en el hospital; <br></br> con esto evitamos la propagación de enfermedades respiratorias.</span>  
+                <span style={{fontSize:"14px", marginLeft:"100px", color:"white"}}>¡Seguimos cuidando tu salud! Recuerda: el uso de cubrebocas es obligatorio durante tu estancia en el hospital; <br></br> con esto evitamos la propagación de enfermedades respiratorias.<i class="fa-solid fa-mask-face"></i></span>  
                 <div
                     className="collapse navbar-collapse"
                     id="navbarNav"
